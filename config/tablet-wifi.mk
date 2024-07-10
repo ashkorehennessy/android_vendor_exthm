f93bfcf0f57ecdce4bb8f589babcc4fd473ca0c0
Yuyuko1024 <dingwenxuan4@gmail.com>
2024-07-10 18:54:46 +0800

    config: Inherit Window Extensions for all tablet wifi devices

--- config/tablet-wifi.mk
+++ config/tablet-wifi.mk
@@ -7,6 +7,9 @@
 # Inherit common exTHmUI stuff
 $(call inherit-product, vendor/exthm/config/common.mk)
 
+# Inherit Window Extensions
+$(call inherit-product, $(SRC_TARGET_DIR)/product/window_extensions.mk)
+
 # Telephony packages
 PRODUCT_PACKAGES += \
     Updater
